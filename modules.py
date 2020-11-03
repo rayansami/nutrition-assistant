61448a94e013c88353d45265fdf7229d04375ddd
Ibrokhim Iskandarov <ibrokhim.iskandarov@gmail.com>
2020-11-03 15:56:09 -0800

    Still in progress

--- modules.py
+++ modules.py
@@ -1,16 +1,15 @@
 import speech_recognition as sr
 import pyttsx3
 
-def speechToText(r):
+def speechToText(r, source2):
     try:
-        with sr.Microphone() as source2:
-            r.adjust_for_ambient_noise(source2, duration=0.2)
-            audio2 = r.listen(source2)
-            myText = r.recognize_google(audio2)
-            myText = myText.lower()
-            return myText
+        r.adjust_for_ambient_noise(source2, duration=0.2)
+        audio2 = r.listen(source2)
+        myText = r.recognize_google(audio2)
+        myText = myText.lower()
+        return myText
                 
-            #print("Did you say " + myText)
+        #print("Did you say " + myText)
     except sr.RequestError as e:
         print("Could not request results; {0}".format(e))
             
